501f8e49adde581acf2750e789a08d8b1053901f
Wahel <wahel.gader27@gmail.com>
2025-04-03 09:36:39 +0200

    update credentials

--- server/model.php
+++ server/model.php
@@ -14,6 +14,6 @@
  * DBPWD : Mot de passe pour se connecter à la base de données.
  */
 define("HOST", "localhost");
-define("DBNAME", "mora");
-define("DBLOGIN", "root");
-define("DBPWD", "root");
+define("DBNAME", "gader3");
+define("DBLOGIN", "gader3");
+define("DBPWD", "gader3");
